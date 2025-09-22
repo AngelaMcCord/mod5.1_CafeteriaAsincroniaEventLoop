30e76c995ea92d531b1554f0bc22e9e371314919
AngelaMc <angela.corderoconde@gmail.com>
2025-09-22 10:18:18 -0400

    agrega setTimeout de pedidos

--- main.js
+++ main.js
@@ -22,3 +22,41 @@ function updateOrderStatus(order, status) {
         listItem.textContent = `Pedido #${order.id}: ${status}`;
     }
 }
+
+    // TODO: Simular la preparación del pedido usando setTimeout y Promise
+    // TODO: Actualizar el estado del pedido a "Completado"
+
+function processOrder(order) {
+
+    //Etapa 1: En preparación (después de 1 seg)
+    setTimeout(()=> {
+        order.status = 'En preparación';
+        updateOrderStatus(order, order.status);
+    }, 1000);
+
+    //Etapa 2: Cocinando (después de 3 seg en total)
+        setTimeout(()=> {
+        order.status = 'Cocinando';
+        updateOrderStatus(order, order.status);
+    }, 3000);
+
+     //Etapa 3: Empacando (después de 5 seg en total)
+        setTimeout(()=> {
+        order.status = 'Empacando';
+        updateOrderStatus(order, order.status);
+    }, 5000);
+
+     //Etapa Final: Completado (después de 6 seg en total)        
+        setTimeout(() => {
+        order.status = 'Completado';
+        updateOrderStatus(order, order.status);
+
+        //Agregar un efecto visual cuando se complete
+        const listItem = document.getElementById(`order-${order.id}`);
+        if (listItem) {
+            listItem.style.backgroundColor = '#d4edda';
+            listItem.style.color = '#155724';
+            listItem.style.fontWeight = 'bold';
+        }
+    }, 6000);
+}
